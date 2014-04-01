9d35521fde85e9fcaafe2cbfd8b3dc46a5649a9b
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-04-01 17:29:17 +0800

    adjust links args

--- PNChartBindings/libPNChart.linkwith.cs
+++ PNChartBindings/libPNChart.linkwith.cs
@@ -1,4 +1,9 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libPNChart.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith (
+    "libPNChart.a",
+    LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator,
+    Frameworks = "Foundation,UIKit,CoreGraphics,QuartzCore",
+    ForceLoad = true
+)]
