6847980134fa3587dd4161d98054db090766dba6
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-04-02 11:34:22 +0800

    add demos

---
+++ PNChartDemos/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace PNChartDemos {
+
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate {
+
+        UIWindow window;
+        UIStoryboard storyboard;
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions) {
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            storyboard = UIStoryboard.FromName("PNChartStoryboard", null);
+            window.RootViewController = (UIViewController)storyboard.InstantiateInitialViewController();
+            window.MakeKeyAndVisible();
+            return true;
+        }
+    }
+}
+
